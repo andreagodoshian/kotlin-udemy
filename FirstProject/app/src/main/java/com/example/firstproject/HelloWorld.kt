0895295fbfaee5a52cd834d74fb91f633f157799
Andrea Godoshian <94014358+godoshian@users.noreply.github.com>
2022-08-10 13:35:30 -0400

    Hallo Welt! - Process finished with exit code 0

---
+++ FirstProject/app/src/main/java/com/example/firstproject/HelloWorld.kt
@@ -0,0 +1,10 @@
+package com.example.firstproject
+
+// "you don't need a class in Kotlin"
+// ...but you do still need main
+fun main(args:Array<String>) {
+    // PUBLIC! STATIC! VOID! MAIN! STRING! ARGS!
+
+    print("Hallo Welt!")
+
+}
