8238f94c953316f85a758a23461b7a6c92e1bd8c
Andrea Godoshian <94014358+godoshian@users.noreply.github.com>
2022-08-17 11:09:19 -0400

    learning - constraint layout

--- FirstAndroidAttempt/app/src/main/java/com/example/firstandroidattempt/MainActivity.kt
+++ FirstAndroidAttempt/app/src/main/java/com/example/firstandroidattempt/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.firstandroidattempt
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var myText:TextView
+    // var myText:TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        myText = findViewById(R.id.textExamlpe)
+        // "R" stands for resources
+
+        myText.setTextColor(Color.CYAN)
+        myText.setText("This is an example from MainActivity!")
+
+        myText.setOnClickListener {
+            myText.setTextColor(Color.MAGENTA)
+            myText.setBackgroundColor(Color.BLUE)
+        }
     }
+
 }
