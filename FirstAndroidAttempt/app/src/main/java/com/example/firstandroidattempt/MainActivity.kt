f449168173bc35d946ef4ad88b550d86100325df
Andrea Godoshian <94014358+godoshian@users.noreply.github.com>
2022-08-17 11:41:20 -0400

    learning - buttons

--- FirstAndroidAttempt/app/src/main/java/com/example/firstandroidattempt/MainActivity.kt
+++ FirstAndroidAttempt/app/src/main/java/com/example/firstandroidattempt/MainActivity.kt
@@ -3,19 +3,26 @@ package com.example.firstandroidattempt
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var myText:TextView
     // var myText:TextView? = null
 
+    lateinit var button1:Button
+    lateinit var button2:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // "R" stands for resources
 
         myText = findViewById(R.id.textExamlpe)
-        // "R" stands for resources
+        button1 = findViewById(R.id.button1)
+        button2 = findViewById(R.id.button2)
 
         myText.setTextColor(Color.CYAN)
         myText.setText("This is an example from MainActivity!")
@@ -24,6 +31,18 @@ class MainActivity : AppCompatActivity() {
             myText.setTextColor(Color.MAGENTA)
             myText.setBackgroundColor(Color.BLUE)
         }
+
+        button1.setOnClickListener {
+            button1.setTextColor(Color.YELLOW)
+            button1.setBackgroundColor(Color.BLACK)
+            button1.setText("Sleepy...")
+            myText.isVisible = false
+        }
+
+        button2.setOnClickListener {
+            myText.isVisible = true
+        }
+
     }
 
 }
